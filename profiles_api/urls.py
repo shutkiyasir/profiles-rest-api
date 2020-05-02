e4c66f271fb30904e9ee9361d23ca86d11ab6a59
Yasir <39209492+shutkiyasir@users.noreply.github.com>
2020-05-02 19:52:12 +0600

    Added user profile viewset and permission

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from profiles_api.views import HelloAPIView, HelloViewSet
+from profiles_api.views import HelloAPIView, HelloViewSet, UserProfileViewSet
 
 
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
+router.register('profile', UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', HelloAPIView.as_view()),
